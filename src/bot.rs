c3775bebc6f7dedb564755a17ec7579ad53154c0
krapmatt <matesbrejcha@gmail.com>
2024-06-18 20:22:12 +0200

    BungieName command added

--- src/bot.rs
+++ src/bot.rs
@@ -1,6 +1,5 @@
 use crate::{ 
-    bot_commands::{ban_bots, handle_join, handle_leave, handle_next, handle_pos, handle_queue, handle_remove, is_follower, is_moderator, register_user, simple_command}, 
-    database::QUEUE_TABLE, initialize_database
+    bot_commands::{ban_bots, bungiename, handle_join, handle_leave, handle_next, handle_pos, handle_queue, handle_remove, is_follower, is_moderator, is_valid_bungie_name, register_user, simple_command}, database::QUEUE_TABLE, initialize_database, AppState, ChatMessage, SharedState
 };
 use dotenv::dotenv;
 use tmi::Client;
@@ -39,15 +38,23 @@ impl BotState {
         client
     }
 }
-
-pub async fn run_chat_bot(bot_state: Arc<Mutex<BotState>>) -> anyhow::Result<()> {
+//Add command command for mods
+//Timers/Counters
+// !bungiename
+//Bungie api stuff - evade it
+pub async fn run_chat_bot(bot_state: Arc<Mutex<BotState>>, shared_state: Arc<std::sync::Mutex<SharedState>>,) -> anyhow::Result<()> {
     let mut client = bot_state.lock().await.client_builder().await;
 
     loop {
         let msg = client.recv().await?;
         match msg.as_typed()? {
             tmi::Message::Privmsg(msg) => {
-                
+                let chat_message = ChatMessage {
+                    channel: msg.channel().to_string(),
+                    user: msg.sender().name().to_string(),
+                    text: msg.text().to_string(),
+                };
+                shared_state.lock().unwrap().add_message(chat_message);
                 
                 //temp
                 //set size of queue
@@ -57,47 +64,74 @@ pub async fn run_chat_bot(bot_state: Arc<Mutex<BotState>>) -> anyhow::Result<()>
                 let conn = Mutex::new(initialize_database("queue.db", QUEUE_TABLE).unwrap());
                 println!("Channel: {}, {}: {}", msg.channel() ,msg.sender().name(), msg.text());
                 let mut bot_state = bot_state.lock().await;
-                if msg.text().starts_with("!open_queue") && is_moderator(&msg, &mut client).await {
-                    
-                    conn.lock().await.execute("DELETE from queue", [])?;
-                    bot_state.queue_open = true;
-                    client.privmsg(msg.channel(), "The queue is now open!").send().await?;
-                } else if msg.text().starts_with("!close_queue") && is_moderator(&msg, &mut client).await {
-                    
-                    bot_state.queue_open = false;
-                    client.privmsg(msg.channel(), "The queue is now closed!").send().await?;
-                } else if bot_state.queue_open {
-                    if msg.text().starts_with("!join") && is_follower(&msg, &mut client, &bot_state.oauth_token_bot, bot_state.bot_id.clone()).await {
-                        handle_join(&msg, &mut client, queue_len, &conn).await?;
-                    } else if msg.text().starts_with("!next") && is_moderator(&msg, &mut client).await {
-                        handle_next(&msg, &mut client,queue_drop, &conn).await?;
-                    } else if msg.text().starts_with("!remove") && is_moderator(&msg, &mut client).await {
-                        handle_remove(&msg, &mut client, &conn).await?;
-                    } else if msg.text().starts_with("!pos") {
-                        handle_pos(&msg, &mut client, queue_len, &conn).await?;
-                    } else if msg.text().starts_with("!leave") {
-                        handle_leave(&msg, &mut client, &conn).await?;
-                    } else if msg.text().starts_with("!queue") {
-                        handle_queue(&msg, &mut client, &conn).await?;
+                match msg.text() {
+                    text if text.starts_with("!open_queue") && is_moderator(&msg, &mut client).await => {
+                        conn.lock().await.execute("DELETE from queue", [])?;
+                        bot_state.queue_open = true;
+                        client.privmsg(msg.channel(), "The queue is now open!").send().await?;
                     }
-                } else {
-                    if msg.text().starts_with("!join") || msg.text().starts_with("!next") || msg.text().starts_with("!remove") || msg.text().starts_with("!pos") || msg.text().starts_with("!leave") || msg.text().starts_with("!queue") {
+                    text if text.starts_with("!close_queue") && is_moderator(&msg, &mut client).await => {
+                        bot_state.queue_open = false;
+                        client.privmsg(msg.channel(), "The queue is now closed!").send().await?;
+                    }
+                    text if bot_state.queue_open => match text {
+                        text if text.starts_with("!join") && is_follower(&msg, &mut client, &bot_state.oauth_token_bot, bot_state.bot_id.clone()).await => {
+                            handle_join(&msg, &mut client, queue_len, &conn).await?;
+                        }
+                        text if text.starts_with("!next") && is_moderator(&msg, &mut client).await => {
+                            handle_next(&msg, &mut client, queue_drop, &conn).await?;
+                        }
+                        text if text.starts_with("!remove") && is_moderator(&msg, &mut client).await => {
+                            handle_remove(&msg, &mut client, &conn).await?;
+                        }
+                        text if text.starts_with("!pos") => {
+                            handle_pos(&msg, &mut client, queue_len, &conn).await?;
+                        }
+                        text if text.starts_with("!leave") => {
+                            handle_leave(&msg, &mut client, &conn).await?;
+                        }
+                        text if text.starts_with("!queue") => {
+                            handle_queue(&msg, &mut client, &conn).await?;
+                        }
+                        _ => {}
+                    }
+                    text if text.starts_with("!join") || text.starts_with("!next") || text.starts_with("!remove") || text.starts_with("!pos") || text.starts_with("!leave") || text.starts_with("!queue") => {
                         client.privmsg(msg.channel(), "The queue is currently closed!").send().await?;
                     }
+                    text if text.starts_with("!id") => {
+                        simple_command(&msg, &mut client, "Type in game chat: /join KrapMatt#1497").await?;
+                    }
+                    text if text.starts_with("!discord") => {
+                        simple_command(&msg, &mut client, "https://discord.gg/jJMwaetjeu").await?;
+                    }
+                    text if text.starts_with("!lurk") => {
+                        simple_command(&msg, &mut client, &format!("Thanks for the lurk {}. I'll appreciate if you leave tab open <3", msg.sender().name())).await?;
+                    }
+                    text if text.starts_with("!register") => {
+                        register_user(&msg, &mut client).await;
+                    }
+                    text if text.starts_with("Cheap viewers on u.to/") || text.starts_with("Best viewers on cutt.ly/") => {
+                        ban_bots(&msg, &mut client, &bot_state.oauth_token_bot, bot_state.bot_id.clone()).await;
+                        client.privmsg(msg.channel(), "We don't want cheap viewers, only expensive ones <3").send().await?;
+                    }
+                    text if text.starts_with("!blameArr") => {
+                        simple_command(&msg, &mut client, "If something doesn't work, blame Arr").await?;
+                    }
+                    text if text.starts_with("!bungiename") => {
+                        if text.trim_end().len() == 11 {
+                            bungiename(&msg, &mut client, &msg.sender().name()).await?;
+                        } else {
+                            let (_, twitch_name) = text.split_once(" ").expect("How did it panic, what happened? //Always is something here");
+                            let mut twitch_name = twitch_name.to_string();
+                            if twitch_name.starts_with("@") {
+                                twitch_name.remove(0);
+                            }
+                            bungiename(&msg, &mut client, &twitch_name).await?;
+                        }
+
+                    }
+                    _ => {}
                 }
-                
-                if msg.text().starts_with("!id") {
-                    simple_command(&msg, &mut client, "Type in game chat: /join KrapMatt#1497").await?;
-                } else if msg.text().starts_with("!discord") {
-                    simple_command(&msg, &mut client, "https://discord.gg/jJMwaetjeu").await?;
-                } else if msg.text().starts_with("!lurk") {
-                    simple_command(&msg, &mut client, &format!("Thanks for the lurk {}. I'll appreciate if you leave tab open <3", msg.sender().name())).await?;
-                } else if msg.text().starts_with("!register") {
-                    register_user(&msg, &mut client).await;
-                } else if msg.text().starts_with("Cheap viewers on u.to/") {
-                    ban_bots(&msg, &mut client, &bot_state.oauth_token_bot, bot_state.bot_id.clone()).await;
-                }
-                
             }
             tmi::Message::Reconnect => {
                 client.reconnect().await?;
@@ -110,3 +144,4 @@ pub async fn run_chat_bot(bot_state: Arc<Mutex<BotState>>) -> anyhow::Result<()>
         }
     }
 }
+
